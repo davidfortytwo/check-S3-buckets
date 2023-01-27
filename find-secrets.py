011528a79ad58f4fe717e52f541a86a0d9fef830
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2023-01-27 15:20:44 +0100

    Update find-secrets.py

--- find-secrets.py
+++ find-secrets.py
@@ -1,5 +1,7 @@
 import boto3
+import os
 import re
+import psutil
 from tqdm import tqdm
 
 # Create an S3 client
@@ -11,6 +13,23 @@ secrets = [r'password', r'secret']
 # Get a list of all S3 buckets
 response = s3.list_buckets()
 buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Get the total size of all objects in all buckets
+total_size = 0
+for bucket in buckets:
+    paginator = s3.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket=bucket):
+        for obj in result.get('Contents', []):
+            total_size += obj['Size']
+
+# Check if there is enough free space on the local disk
+free_space = psutil.disk_usage('/').free
+if total_size > free_space:
+    print(f'You need at least {total_size} bytes of free space to download all objects.')
+    print(f'You have {free_space} bytes of free space.')
+    proceed = input('Do you want to proceed? (y/n)')
+    if proceed.lower() != 'y':
+        exit()
 
 # Iterate through each bucket
 for bucket in tqdm(buckets):
