c16ff1f89f136144f868ff1f2ef0eb10177886b0
cbk914 <cbk914@riseup.net>
2023-01-28 12:02:56 +0100

    Update get-s3-objects.py

--- get-s3-objects.py
+++ get-s3-objects.py
@@ -1,11 +1,20 @@
 import boto3
 import logging
 
+# Create a logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-file_name = "s3_objects.txt"
-logging.basicConfig(filename="get_s3_objects.log", level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+# Create a file handler
+file_handler = logging.FileHandler("get_s3_objects.log")
+file_handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the file handler
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
 
 
 # Create an S3 client with the IAM role of the instance
@@ -18,13 +27,20 @@ buckets = [bucket['Name'] for bucket in response['Buckets']]
 logger.info("Finished listing S3 buckets")
 
 # Open a file to write the objects
-with open(file_name, 'w') as f:
-    # Iterate through each bucket and list all objects
-    for bucket in buckets:
-        logger.info(f"Starting to list objects in {bucket}")
-        # Use the boto3 paginator to handle pagination for the list_objects_v2 method
-        paginator = s3.get_paginator('list_objects_v2')
-        for result in paginator.paginate(Bucket=bucket):
-            for obj in result.get('Contents', []):
-                f.write(obj['Key'] + '\n')
-        logger.info(f"Finished listing objects in {bucket}")
+try:
+    # Open a file to write the objects
+    with open("get-s3-objects.txt", 'w') as f:
+        # Iterate through each bucket and list all objects
+        for bucket in buckets:
+            logger.info(f"Starting to list objects in {bucket}")
+            # Use the boto3 paginator to handle pagination for the list_objects_v2 method
+            paginator = s3.get_paginator('list_objects_v2')
+            for result in paginator.paginate(Bucket=bucket):
+                for obj in result.get('Contents', []):
+                    f.write(obj['Key'] + '\n')
+            logger.info(f"Finished listing objects in {bucket}")
+except IOError as e:
+    logger.error("An error occurred while opening or writing to the file: " + str(e))
+        
+finally:
+    file_handler.close() 
